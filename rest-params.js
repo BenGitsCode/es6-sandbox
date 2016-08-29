984f8eea1476bc21410f03fab17afa238fb4562a
benjamski <benjadamski@gmail.com>
2016-08-28 21:14:09 -0400

    Rest Parameters
    Rest paramters examples

---
+++ rest-params.js
@@ -0,0 +1,36 @@
+(function(){
+    "use strict";
+
+    // let sum = function() {
+    //  console.log(arguments);
+    // };
+
+    // let sum = function() {
+    //  return Array.prototype.reduce.call(arguments,((prev,curr) => {
+    //   return prev + curr;
+    //  });
+    // };
+
+
+    // let sum = function(...args) {
+    //  return args.reduce((prev,curr) => prev + curr);
+    // };
+    //
+    // console.log(sum(2,3,4,5) );
+
+    let multiply = (mul, ...numbers) => {
+     console.log(mul, numbers);
+    };
+
+    multiply(2,7,4,5);
+
+
+
+
+
+
+
+
+
+
+})();
